830138d630960834062547a9fdadc80ad7347ce1
raven003 <xzardos@web.de>
2018-01-12 01:02:13 +0100

    somecomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test/test.component';
+import { EventsComponent } from './events/events.component';
+import { PicturesComponent } from './pictures/pictures.component';
+import { CleanComponent } from './clean/clean.component';
+import { ShoppingComponent } from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    EventsComponent,
+    PicturesComponent,
+    CleanComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule
